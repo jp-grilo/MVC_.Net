766fb97c61a20892d738be21987f87458cb69bbe
jp-grilo <joaoplopescg@gmail.com>
2025-11-11 11:46:27 -0300

    Adicionar arquivos de projeto.

---
+++ MVC .Net/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_.Net.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
